13389f3f39151f127d40736c4b3df62548614e4c
Benjamin Norrington <ben@norrington.net>
2015-06-17 22:20:21 +0100

    replaced tabs with spaces

--- index.js
+++ index.js
@@ -40,15 +40,15 @@ var downS   = {op:DOWN, kind:""};
 
 
 function r(n) {
-	return {op:RETAIN, n:n};
+  return {op:RETAIN, n:n};
 }
 
 function i(vs) {
-	return {op:INSERT, values:vs};
+  return {op:INSERT, values:vs};
 }
 
 function d(vs) {
-	return {op:DELETE, values:vs};
+  return {op:DELETE, values:vs};
 }
 
 var doca = [doc,[],[p,[],"Hello, World!"]];
@@ -58,7 +58,7 @@ var docb = [doc,[],[p,[],'Hello, ',[bold,[],'World!']]];
 //we can serialise this into a simple form and generate it pretty easily.
 var lopa = [upA,r(2),upA,r(2),upS,r(7),i("Cruel "),r(6),downS,downA,downA];
 var lopb = [upA,r(2),upA,r(2),upS,r(7),
-	popS,pushA,i(["bold",[]]),pushS,r(6),popA,downS,downA,downA];
+  popS,pushA,i(["bold",[]]),pushS,r(6),popA,downS,downA,downA];
 
 //we might have the pops the wrong way up... (at the moment we don't check kind)
 
@@ -69,8 +69,8 @@ var lopb = [upA,r(2),upA,r(2),upS,r(7),
 // [i(a),i(b)] -> [i(a+b)] //where + is concat if array.
 
 function push(ops, op) {
-	//TODO (check rules first)
-	return ops.push(op);
+  //TODO (check rules first)
+  return ops.push(op);
 }
 
 /*
@@ -94,215 +94,215 @@ var opa = [r(2),[r(2),[r(7),i("Cruel "),r(6)]]];
 var opb = [r(2),[r(2),[r(7),popS,pushA,i(["bold",[]]),pushS,r(6),popA]]];
 
 function invert(ops) {
-	function invertOp(op) {
-		if (Array.isArray(op)) return invert(op);
-		switch (op.op) {
-			case RETAIN: return op;
-			case INSERT: return d(op.values);
-			case DELETE: return i(op.values);
-			case PUSH:
-				return (typeof op.kind === 'string') ? unpushS : unpushA;
-			case POP:
-				return (typeof op.kind === 'string') ? unpopS : unpopA;
-			case UNPUSH: 
-				return (typeof op.kind === 'string') ? pushS : pushA;
-			case UNPOP:
-				return (typeof op.kind === 'string') ? popS : popA;
-			default:
-				return op;
-		}
-	}
-	return ops.map(invertOp);
+  function invertOp(op) {
+    if (Array.isArray(op)) return invert(op);
+    switch (op.op) {
+      case RETAIN: return op;
+      case INSERT: return d(op.values);
+      case DELETE: return i(op.values);
+      case PUSH:
+        return (typeof op.kind === 'string') ? unpushS : unpushA;
+      case POP:
+        return (typeof op.kind === 'string') ? unpopS : unpopA;
+      case UNPUSH: 
+        return (typeof op.kind === 'string') ? pushS : pushA;
+      case UNPOP:
+        return (typeof op.kind === 'string') ? popS : popA;
+      default:
+        return op;
+    }
+  }
+  return ops.map(invertOp);
 }
 
 function makeAppend(result) {
-	return function(op) { 
-		return push(result, op); 
-	};
+  return function(op) { 
+    return push(result, op); 
+  };
 }
 
 function compose(opA, opB) {
-	var result = [];
+  var result = [];
 
-	var append = makeAppend(result);
-	//TODO
-	return result;
+  var append = makeAppend(result);
+  //TODO
+  return result;
 }
 
 function transform(op1, op2, side) {
-	var left = side == 'left';
-	//TODO
-	return op1;
+  var left = side == 'left';
+  //TODO
+  return op1;
 }
 
 function applyl(d,ops) {
-	var stack = []
-	var t = [];
-	var docStack = [];
-	var dp = [d]; //wrap the doc so the first upA gets it.
-	var o = 0;
+  var stack = []
+  var t = [];
+  var docStack = [];
+  var dp = [d]; //wrap the doc so the first upA gets it.
+  var o = 0;
 
-	function append(vals) {
-		if (typeof t === 'string') {
-			t += vals
-		} else {
-			t = t.concat(vals);
-		}
-	}
+  function append(vals) {
+    if (typeof t === 'string') {
+      t += vals
+    } else {
+      t = t.concat(vals);
+    }
+  }
 
-	for (var i = 0; i < ops.length; i++) {
-		var op = ops[i];
-		switch (op.op) {
-			case INSERT:
-				append(op.values);
-				break;
-			case RETAIN:
-				append(dp.slice(o,o+op.n));
-				o += op.n;
-				break;
-			case PUSH:
-				stack.push(t);
-				t = (typeof op.kind === 'string') ? "" : [];
-				break;
-			case POP:
-				var temp = t;
-				t = stack.pop();
-				if (typeof t === 'string')
-					t += temp;
-				else
-					t.push(temp);
-				break;
-			case DELETE:
-				o += op.values.length;
-				break;
-			case UNPUSH:
-				//move deeper into the doc
-				docStack.push({o:o, dp:dp});
-				dp = dp[o];
-				o = 0;
-				break;
-			case UNPOP:
-				//move shallower in the doc
-				var _d = docStack.pop();
-				dp = _d.dp;
-				o = _d.o + 1;
-				break;
-			case UP:
-				stack.push(t);
-				t = (typeof op.kind === 'string') ? "" : [];
-				docStack.push({o:o, dp:dp});
-				dp = dp[o];
-				o = 0;
-				break;
-			case DOWN:
-				//move shallower in the doc
-				var _d = docStack.pop();
-				dp = _d.dp;
-				o = _d.o + 1;
-				var temp = t;
-				t = stack.pop();
-				if (typeof t === 'string')
-					t += temp;
-				else
-					t.push(temp);
-				break;
+  for (var i = 0; i < ops.length; i++) {
+    var op = ops[i];
+    switch (op.op) {
+      case INSERT:
+        append(op.values);
+        break;
+      case RETAIN:
+        append(dp.slice(o,o+op.n));
+        o += op.n;
+        break;
+      case PUSH:
+        stack.push(t);
+        t = (typeof op.kind === 'string') ? "" : [];
+        break;
+      case POP:
+        var temp = t;
+        t = stack.pop();
+        if (typeof t === 'string')
+          t += temp;
+        else
+          t.push(temp);
+        break;
+      case DELETE:
+        o += op.values.length;
+        break;
+      case UNPUSH:
+        //move deeper into the doc
+        docStack.push({o:o, dp:dp});
+        dp = dp[o];
+        o = 0;
+        break;
+      case UNPOP:
+        //move shallower in the doc
+        var _d = docStack.pop();
+        dp = _d.dp;
+        o = _d.o + 1;
+        break;
+      case UP:
+        stack.push(t);
+        t = (typeof op.kind === 'string') ? "" : [];
+        docStack.push({o:o, dp:dp});
+        dp = dp[o];
+        o = 0;
+        break;
+      case DOWN:
+        //move shallower in the doc
+        var _d = docStack.pop();
+        dp = _d.dp;
+        o = _d.o + 1;
+        var temp = t;
+        t = stack.pop();
+        if (typeof t === 'string')
+          t += temp;
+        else
+          t.push(temp);
+        break;
 
-		}
-	};
+    }
+  };
 
-	return t[0];
+  return t[0];
 }
 
 function apply(d,ops) {
-	var stack = []
-	var t = [];
-	var docStack = [];
-	var dp = d;
-	var o = 0;
+  var stack = []
+  var t = [];
+  var docStack = [];
+  var dp = d;
+  var o = 0;
 
-	//IDEA: unroll the tree into a linear POP UNPOP solution.
-	//it might be easier to do things like compose that way.
-	//and we can always roll it up again at the end.
+  //IDEA: unroll the tree into a linear POP UNPOP solution.
+  //it might be easier to do things like compose that way.
+  //and we can always roll it up again at the end.
 
-	//The reason that it is hard to unroll is that we don't currently
-	//distinguish between string and not string so we would need the
-	//doc to know how to unroll.
+  //The reason that it is hard to unroll is that we don't currently
+  //distinguish between string and not string so we would need the
+  //doc to know how to unroll.
 
-	//could store the op like that though.
+  //could store the op like that though.
 
-	function append(vals) {
-		if (typeof t === 'string') {
-			t += vals
-		} else {
-			t = t.concat(vals);
-		}
-	}
+  function append(vals) {
+    if (typeof t === 'string') {
+      t += vals
+    } else {
+      t = t.concat(vals);
+    }
+  }
 
-	function up(opy) {
-		for (var i = 0; i < opy.length; i++) {
-			var op = opy[i];
-			if (Array.isArray(op)) {
-				//push
-				stack.push(t)
-				t =  (typeof dp[o] === 'string') ? "" : [];
-				//unpush
-				docStack.push({o:o, dp:dp});
-				dp = dp[o];
-				o = 0;
-				//up
-				up(op);
-				//unpop
-				var _d = docStack.pop();
-				dp = _d.dp;
-				o = _d.o + 1;
-				//pop
-				var temp = t;
-				t = stack.pop();
-				if (typeof t === 'string')
-					t += temp;
-				else
-					t.push(temp);
-				continue;
-			}
-			switch (op.op) {
-				case INSERT:
-					append(op.values);
-					break;
-				case RETAIN:
-					append(dp.slice(o,o+op.n));
-					o += op.n;
-					break;
-				case PUSH:
-					stack.push(t);
-					t = (typeof op.kind === 'string') ? "" : [];
-					break;
-				case POP:
-					var temp = t;
-					t = stack.pop();
-					if (typeof t === 'string')
-						t += temp;
-					else
-						t.push(temp);
-					break;
-				case DELETE:
-					o += op.values.length;
-					break;
-				case UNPUSH:
-					//move deeper into the doc
-					docStack.push({o:o, dp:dp});
-					dp = dp[o];
-					o = 0;
-					break;
-				case UNPOP:
-					//move shallower in the doc
-					var _d = docStack.pop();
-					dp = _d.dp;
-					o = _d.o + 1;
-					break;
-			}
-		};
-	}
+  function up(opy) {
+    for (var i = 0; i < opy.length; i++) {
+      var op = opy[i];
+      if (Array.isArray(op)) {
+        //push
+        stack.push(t)
+        t =  (typeof dp[o] === 'string') ? "" : [];
+        //unpush
+        docStack.push({o:o, dp:dp});
+        dp = dp[o];
+        o = 0;
+        //up
+        up(op);
+        //unpop
+        var _d = docStack.pop();
+        dp = _d.dp;
+        o = _d.o + 1;
+        //pop
+        var temp = t;
+        t = stack.pop();
+        if (typeof t === 'string')
+          t += temp;
+        else
+          t.push(temp);
+        continue;
+      }
+      switch (op.op) {
+        case INSERT:
+          append(op.values);
+          break;
+        case RETAIN:
+          append(dp.slice(o,o+op.n));
+          o += op.n;
+          break;
+        case PUSH:
+          stack.push(t);
+          t = (typeof op.kind === 'string') ? "" : [];
+          break;
+        case POP:
+          var temp = t;
+          t = stack.pop();
+          if (typeof t === 'string')
+            t += temp;
+          else
+            t.push(temp);
+          break;
+        case DELETE:
+          o += op.values.length;
+          break;
+        case UNPUSH:
+          //move deeper into the doc
+          docStack.push({o:o, dp:dp});
+          dp = dp[o];
+          o = 0;
+          break;
+        case UNPOP:
+          //move shallower in the doc
+          var _d = docStack.pop();
+          dp = _d.dp;
+          o = _d.o + 1;
+          break;
+      }
+    };
+  }
 
-	up(ops);
-	return t;
+  up(ops);
+  return t;
 }
