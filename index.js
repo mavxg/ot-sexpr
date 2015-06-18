1a36878216628193995229308b170df389bf45f0
Benjamin Norrington <ben@norrington.net>
2015-06-18 10:47:16 +0100

    remove old nested ops and kind from pop and down

--- index.js
+++ index.js
@@ -29,10 +29,8 @@ var DOWN    = "down";
 var START   = "start";
 var END     = "end";
 
-var popA    = {op:POP, kind:""};
-var popS    = {op:POP, kind:[]};
-var unpopA  = {op:UNPOP, kind:""};
-var unpopS  = {op:UNPOP, kind:[]};
+var pop     = {op:POP};
+var unpop   = {op:UNPOP};
 var pushA   = {op:PUSH, kind:[]};
 var pushS   = {op:PUSH, kind:""};
 var unpushA = {op:UNPUSH, kind:[]};
@@ -41,8 +39,7 @@ var unpushS = {op:UNPUSH, kind:""};
 //linearisation
 var upA     = {op:UP, kind:[]};
 var upS     = {op:UP, kind:""};
-var downA   = {op:DOWN, kind:[]};
-var downS   = {op:DOWN, kind:""};
+var down    = {op:DOWN};
 
 //critical regions
 var start   = {op:START};
@@ -66,9 +63,16 @@ var docb = [doc,[],[p,[],'Hello, ',[bold,[],'World!']]];
 
 //IDEA: linearisation - might be easier to do things like compose and transform.
 //we can serialise this into a simple form and generate it pretty easily.
-var lopa = [upA,r(2),upA,r(2),upS,r(7),i("Cruel "),r(6),downS,downA,downA];
-var lopb = [upA,r(2),upA,r(2),upS,r(7),
-  popS,pushA,i(["bold",[]]),pushS,r(6),popA,downS,downA,downA];
+var opa = [upA,r(2),upA,r(2),upS,r(7),i("Cruel "),r(6),down,down,down];
+var opb = [upA,r(2),upA,r(2),upS,r(7),
+  pop,pushA,i(["bold",[]]),pushS,r(6),down,pop,down,down];
+
+
+//target transformed
+var opbp = [upA,r(2),upA,r(2),upS,r(7),
+  pop,pushA,i(["bold",[]]),pushS,r(12),pop,down,down,down];
+var opap = [upA,r(2),upA,r(2),upS,r(7),
+  down,upA,r(2),upS,i("Cruel "),r(6),down,down,down,down];
 
 //we might have the pops the wrong way up... (at the moment we don't check kind)
 
@@ -101,25 +105,18 @@ when we compose it is the unions of the critical regions.
 
 */
 
-var opa = [r(2),[r(2),[r(7),i("Cruel "),r(6)]]];
-//popS has to come where it is to put the string consumed so far into the list
-var opb = [r(2),[r(2),[r(7),popS,pushA,i(["bold",[]]),pushS,r(6),popA]]];
-
 function invert(ops) {
   function invertOp(op) {
-    if (Array.isArray(op)) return invert(op);
     switch (op.op) {
       case RETAIN: return op;
       case INSERT: return d(op.values);
       case DELETE: return i(op.values);
+      case POP:    return unpop;
+      case UNPOP:  return pop;
       case PUSH:
         return (typeof op.kind === 'string') ? unpushS : unpushA;
-      case POP:
-        return (typeof op.kind === 'string') ? unpopS : unpopA;
       case UNPUSH: 
         return (typeof op.kind === 'string') ? pushS : pushA;
-      case UNPOP:
-        return (typeof op.kind === 'string') ? popS : popA;
       default:
         return op;
     }
@@ -220,9 +217,11 @@ function transform(opA, opB, side) {
   return result;
 }
 
-function applyl(d,ops) {
+function apply(d,ops) {
+  //stack and t(op) for traversing output documrny
   var stack = []
   var t = [];
+  //stack and top(dp) for traversing input document
   var docStack = [];
   var dp = [d]; //wrap the doc so we can do [r(1)] and change nothing.
   var o = 0;
@@ -252,10 +251,7 @@ function applyl(d,ops) {
       case POP:
         var temp = t;
         t = stack.pop();
-        if (typeof t === 'string')
-          t += temp;
-        else
-          t.push(temp);
+        t.push(temp);
         break;
       case DELETE:
         o += op.n;
@@ -273,8 +269,10 @@ function applyl(d,ops) {
         o = _d.o + 1;
         break;
       case UP:
+        //move deeper into the output
         stack.push(t);
         t = (typeof op.kind === 'string') ? "" : [];
+        //move deeper into the doc
         docStack.push({o:o, dp:dp});
         dp = dp[o];
         o = 0;
@@ -284,12 +282,10 @@ function applyl(d,ops) {
         var _d = docStack.pop();
         dp = _d.dp;
         o = _d.o + 1;
+        //move shallower in the output
         var temp = t;
         t = stack.pop();
-        if (typeof t === 'string')
-          t += temp;
-        else
-          t.push(temp);
+        t.push(temp);
         break;
       default:
         //do nothing for Start and End.
@@ -298,98 +294,4 @@ function applyl(d,ops) {
   };
 
   return t[0];
-}
-
-function apply(d,ops) {
-  var stack = []
-  var t = [];
-  var docStack = [];
-  var dp = d;
-  var o = 0;
-
-  //IDEA: unroll the tree into a linear POP UNPOP solution.
-  //it might be easier to do things like compose that way.
-  //and we can always roll it up again at the end.
-
-  //The reason that it is hard to unroll is that we don't currently
-  //distinguish between string and not string so we would need the
-  //doc to know how to unroll.
-
-  //could store the op like that though.
-
-  function append(vals) {
-    if (typeof t === 'string') {
-      t += vals
-    } else {
-      t = t.concat(vals);
-    }
-  }
-
-  function up(opy) {
-    for (var i = 0; i < opy.length; i++) {
-      var op = opy[i];
-      if (Array.isArray(op)) {
-        //push
-        stack.push(t)
-        t =  (typeof dp[o] === 'string') ? "" : [];
-        //unpush
-        docStack.push({o:o, dp:dp});
-        dp = dp[o];
-        o = 0;
-        //up
-        up(op);
-        //unpop
-        var _d = docStack.pop();
-        dp = _d.dp;
-        o = _d.o + 1;
-        //pop
-        var temp = t;
-        t = stack.pop();
-        if (typeof t === 'string')
-          t += temp;
-        else
-          t.push(temp);
-        continue;
-      }
-      switch (op.op) {
-        case INSERT:
-          append(op.values);
-          break;
-        case RETAIN:
-          append(dp.slice(o,o+op.n));
-          o += op.n;
-          break;
-        case PUSH:
-          stack.push(t);
-          t = (typeof op.kind === 'string') ? "" : [];
-          break;
-        case POP:
-          var temp = t;
-          t = stack.pop();
-          if (typeof t === 'string')
-            t += temp;
-          else
-            t.push(temp);
-          break;
-        case DELETE:
-          o += op.values.length;
-          break;
-        case UNPUSH:
-          //move deeper into the doc
-          docStack.push({o:o, dp:dp});
-          dp = dp[o];
-          o = 0;
-          break;
-        case UNPOP:
-          //move shallower in the doc
-          var _d = docStack.pop();
-          dp = _d.dp;
-          o = _d.o + 1;
-          break;
-      }
-    };
-  }
-
-  up(ops);
-  return t;
 }
