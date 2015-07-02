9d9b6913422d53ee7db29aa9c054116bb47fe629
Benjamin Norrington <ben@norrington.net>
2015-07-02 09:58:46 +0100

    export list, string, and sym

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ var ot   = require('./lib/operations');
 //extentions
 ot.api   = require('./lib/api');
 ot.parse = require('./lib/parser');
+ot.List  = require('./lib/list');
+ot.sym   = require('./lib/symbol').sym;
+ot.AttributedString = require('./lib/string');
 
 module.exports = ot;
